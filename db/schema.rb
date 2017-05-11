d5b9aba511d7e52fc3b6bee5cdfb9d71ca5f8d08
Eiji Yamamoto <yamamoto-e@swlab.cs.okayama-u.ac.jp>
2017-05-11 06:38:01 +0200

    Modify README.rdoc

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170511025121) do
+ActiveRecord::Schema.define(version: 20170511020531) do
 
   create_table "todolists", force: :cascade do |t|
     t.boolean  "check"
@@ -20,7 +20,6 @@ ActiveRecord::Schema.define(version: 20170511025121) do
     t.date     "deadline"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
-    t.boolean  "done"
   end
 
 end
